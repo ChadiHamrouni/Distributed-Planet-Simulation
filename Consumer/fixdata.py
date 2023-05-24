827513fcdc76336fbd11f10ef533594020e4a868
Chadi Hamrouni <69485266+ChadiHamrouni@users.noreply.github.com>
2023-05-25 00:22:03 +0100

    Update fixdata.py

--- Consumer/fixdata.py
+++ Consumer/fixdata.py
@@ -1,4 +1,4 @@
-def remove_brackets_from_file(file_path):
+def modify_file(file_path):
     # Read the content of the file
     with open(file_path, 'r') as file:
         content = file.read()
@@ -7,34 +7,9 @@ def remove_brackets_from_file(file_path):
     if content[1:3] == '][':
         # Remove the first occurrence of ']['
         modified_content = content.replace('][', '', 1)
-
-        # Write the modified content back to the file
-        with open(file_path, 'w') as file:
-            file.write(modified_content)
-
-
-def remove(file_path):
-    # Read the content of the file
-    with open(file_path, 'r') as file:
-        content = file.read()
-
-    # Check the first three characters
-        # Remove the first occurrence of ']['
-    modified_content = content.replace('][', ',', )
-
-    # Write the modified content back to the file
-    with open(file_path, 'w') as file:
-        file.write(modified_content)
-
-
-def re(file_path):
-    # Read the content of the file
-    with open(file_path, 'r') as file:
-        content = file.read()
-
-    # Check the first three characters
-        # Remove the first occurrence of ']['
-    modified_content = content.replace('}{', '},{', )
+    else:
+        # Replace '}{' with '},{'
+        modified_content = content.replace('}{', '},{')
 
     # Write the modified content back to the file
     with open(file_path, 'w') as file:
@@ -42,6 +17,4 @@ def re(file_path):
 
 
 # Usage
-# remove_brackets_from_file('test.json')
-remove('testing.json')
-# re('test.json')
+modify_file('testing.json')
